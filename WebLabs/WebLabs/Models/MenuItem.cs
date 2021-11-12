b2ce5e5228b4dc50950c52c01bb7c6f951f7cb2e
kalilask4 <kalilask4@gmail.com>
2021-11-12 23:32:26 +0300

    Add class MenuItem
    Add model to describe parameters menu elements

---
+++ WebLabs/WebLabs/Models/MenuItem.cs
@@ -0,0 +1,19 @@
+﻿namespace WebLabs.Models
+{
+    public class MenuItem
+    {
+        public string Text { get; set; }
+        //страница или метод контроллера
+        public bool IsPage { get; set; } = false;
+        public  string ControllerName { get; set; }
+        // имя действия контроллера
+        public  string MethodName { get; set; }
+        // имя действия контроллера
+        public  string PageName { get; set; }
+        // имя области
+        public  string AreaName { get; set; }
+        // класс CSS для текущего пункта меню
+        public  string CssName { get; set; }
+        
+    }
+}

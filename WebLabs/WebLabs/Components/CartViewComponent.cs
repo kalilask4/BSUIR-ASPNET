47c5893f1f4d880be3d7efa6c850feaef9972d93
kalilask4 <kalilask4@gmail.com>
2021-11-13 01:43:43 +0300

    Usini default Cart partial

---
+++ WebLabs/WebLabs/Components/CartViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebLabs.Components
+{
+    public class CartViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
